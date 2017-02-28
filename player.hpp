800edae912a01336e63d4020c18ac4a74512ec25
syfan <tyll1997@gmail.com>
2017-02-28 06:55:51 -0800

    Added variables to player.hpp

--- player.hpp
+++ player.hpp
@@ -13,6 +13,17 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    
+    Side side; // to keep track of side
+    Side opponentSide;
+    
+    int score;
+    int tempscore;
+    Board *board;
+    Board *dupboard;
+    
+    Move *tempmove;
+    Move *newmove;
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
